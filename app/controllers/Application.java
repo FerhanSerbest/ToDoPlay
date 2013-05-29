5f2f79d5ce49ae26c450f03a75131f985b031ed9
FerhanSerbest <ferhan.serbest@gmail.com>
2013-05-29 14:50:26 -0700

    05/29

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,19 +7,38 @@ import play.mvc.*;
 
 import views.html.*;
 
+/**
+ * 
+ * @author Ferhan Serbest
+ * Application class implementing the methods used to manipulate the tasks
+ *
+ */
 public class Application extends Controller {
   
+	
+	/**
+	 * Index method redirecting to the tasks() method
+	 * @return redirection to tasks() method
+	 */
 	public static Result index() {
 		return redirect(routes.Application.tasks());
 	  }
 	  
-	  public static Result tasks() {
-		  return ok(
-				    views.html.index.render(Task.all(), taskForm)
-				  );
-	  }
-	  
-	  public static Result newTask() {
+	
+	/**
+	 * Method rendering the task()
+	 * @return the render for the tasks
+	 */
+	 public static Result tasks() {
+	    return ok(
+			    views.html.index.render(Task.all(), taskForm)
+			  );
+	}
+	 /**
+	  * Method for creating a new task 
+	  * @return redirection to tasks() method
+	  */
+	 public static Result newTask() {
 		  Form<Task> filledForm = taskForm.bindFromRequest();
 		  if(filledForm.hasErrors()) {
 		    return badRequest(
@@ -29,8 +48,12 @@ public class Application extends Controller {
 		    Task.create(filledForm.get());
 		    return redirect(routes.Application.tasks());  
 		  }
-	  }
-	  
+	 }
+	  /**
+	   * Method for deleting a task
+	   * @param id - id of the task to be deleted
+	   * @return redirection to tasks() method
+	   */
 	  public static Result deleteTask(Long id) {
 		  Task.delete(id);
 		  return redirect(routes.Application.tasks());
--- app/models/Task.java
+++ app/models/Task.java
@@ -7,6 +7,11 @@ import play.data.validation.Constraints.*;
 
 import javax.persistence.*;
 
+/**
+ * 
+ * @author Ferhan Serbest
+ * class to implement the properties of a task
+ */
 @Entity
 public class Task extends Model {
     
@@ -20,14 +25,26 @@ public class Task extends Model {
 		    Long.class, Task.class
 		  );
   
+  /**
+   * Method to list tasks
+   * @return uses the Finder to list the tasks
+   */
   public static List<Task> all() {
 	  return find.all();
   }
   
+  /**
+   * Method to create a task
+   * @param task - the task to be created
+   */
   public static void create(Task task) {
 	  task.save();
   }
   
+  /**
+   * Method to delete a task
+   * @param id - id of the task to be deleted
+   */
   public static void delete(Long id) {
 	  find.ref(id).delete();
   }
